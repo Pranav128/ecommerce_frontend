352b931531cef293999d7808e7269babdfc5ae1b
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2025-04-25 01:40:52 +0530

    Update environment.prod.ts
    add railway url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // apiUrl: import.meta.env.NG_APP_API_URL
     // || 'https://ecommerce-api-4u9v.onrender.com'
-  apiUrl: (window as any).env?.API_BASE_URL || 'https://ecommerce28.onrender.com/api'
+  apiUrl: (window as any).env?.API_BASE_URL || 'https://ecommerce-production-7529.up.railway.app/api'
 };
